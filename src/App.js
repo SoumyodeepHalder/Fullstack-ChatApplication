b4abf25dfdebcbc0bc15b5eb607fde59cee0e664
Soumyodeep Halder <soumyodp@gmail.com>
2026-08-10 17:59:21 +0530

    blank screen fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { useState } from 'react';
 import Home from './Home';
@@ -10,13 +10,13 @@ function App() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   return (
-    <BrowserRouter>
+    <HashRouter>
     <Routes>
         <Route path="/" element={<Login username={username} setUsername={setUsername} password={password} setPassword={setPassword}/>} />
         <Route path="/home" element={<Home username={username} password={password}/>} />
         <Route path="/register" element={<Register username={username} setUsername={setUsername} password={password} setPassword={setPassword}/>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
